93863aceb428a6595982ec97c77675ac8917e7e9
Takuya Hirata <hirtak2000@gmail.com>
2019-09-01 00:54:45 +0900

    step7

--- parser.lisp
+++ parser.lisp
@@ -14,7 +14,11 @@
    #:node-kind
    #:node-lhs
    #:node-rhs
-   #:node-val))
+   #:node-val
+   #:ND_BIGGER
+   #:ND_EQ_BIGGER
+   #:ND_EQUAL
+   #:ND_NOT_EQUAL))
 
 (in-package 9lc.parser)
 
@@ -25,30 +29,63 @@
          (when (not (find c '(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)))
            (unless (eq c 'eof)
              (unread-char c is))
-           (return (values (parse-integer (coerce char-lst 'string)) (length char-lst))))
+           (return (values (parse-integer (coerce (nreverse char-lst) 'string)) (length char-lst))))
          (push c char-lst)))))
 
 (defun tokenize (is)
-  (do* ((c (read-char is nil 'eof) (read-char is nil 'eof))
-        (char-cnt 0 (1+ char-cnt))
-        (line-cnt 0 (if (eq c #\Return)
-                        (1+ line-cnt)
-                        line-cnt))
-        tokens)
-       ((eq c 'eof) (nreverse tokens))
-    (let ((token (case c
-                   ((#\  #\Tab) nil)
-                   ((#\+ #\- #\* #\/ #\( #\))
-                    `(TK_RESERVED ,c ,char-cnt))
-                   ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
-                    (unread-char c is)
-                    (multiple-value-bind (num num-char-cnt)
-                        (parse-number is)
-                      (prog1
-                          `(TK_NUM ,num ,char-cnt)
-                        (setf char-cnt (+ char-cnt num-char-cnt)))))
-                   (t (error "トークナイズできません(~a)" char-cnt)))))
-      (push token tokens))))
+  (macrolet ((char-case (&body body)
+               (let ((readed-char (gensym)))
+                 `(let ((,readed-char (read-char is nil 'eof)))
+                    (prog1
+                        (case ,readed-char
+                          ,@(mapcar (lambda (x)
+                                     (if (eq (car x) 't)
+                                         `(t ,@(cdr x))
+                                         `((,(car x)) ,@(cdr x))))
+                                   body))
+                        (incf char-cnt))))))
+    (do* ((c (read-char is nil 'eof) (read-char is nil 'eof))
+          (char-cnt 0 (1+ char-cnt))
+          (line-cnt 0 (if (eq c #\Return)
+                          (1+ line-cnt)
+                          line-cnt))
+          tokens)
+         ((eq c 'eof) (nreverse tokens))
+      (let ((token (case c
+                     ((#\  #\Tab) nil)
+                     ((#\=)
+                      (char-case
+                       (#\= `(TK_RESERVED "==" ,char-cnt))
+                       (t (error "トークナイズできません(~a)" char-cnt))))
+                     ((#\!)
+                      (char-case
+                       (#\= `(TK_RESERVED "!=" ,char-cnt))
+                       (t (error "トークナイズできません(~a)" char-cnt))))
+                     ((#\<)
+                      (char-case
+                       (#\= `(TK_RESERVED "<=" ,char-cnt))
+                       (t (prog2
+                              (unread-char c is)
+                              `(TK_RESERVED "<" ,char-cnt)
+                            (decf char-cnt)))))
+                     ((#\>)
+                      (char-case
+                       (#\= `(TK_RESERVED ">=" ,char-cnt))
+                       (t (prog2
+                              (unread-char c is)
+                              `(TK_RESERVED ">" ,char-cnt)
+                            (decf char-cnt)))))
+                     ((#\+ #\- #\* #\/ #\( #\))
+                      `(TK_RESERVED  ,(string c) ,char-cnt))
+                     ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
+                      (unread-char c is)
+                      (multiple-value-bind (num num-char-cnt)
+                          (parse-number is)
+                        (prog1
+                            `(TK_NUM ,num ,char-cnt)
+                          (setf char-cnt (+ char-cnt num-char-cnt)))))
+                     (t (error "トークナイズできません(~a)" char-cnt)))))
+        (push token tokens)))))
 
 (defstruct anker
   obj)
@@ -68,11 +105,11 @@
 (defmacro if-reserved-token (anker op body-reserved-token body-not-reserved-token)
   `(with-token ,anker
      (if (and (eq (first token) 'TK_RESERVED)
-              (eq (second token) ,op))
+              (equal (second token) ,op))
          ,body-reserved-token
          ,body-not-reserved-token)))
 
-(defun consume (anker op)
+(defun consume (anker op) ;todo need to change for string
   (if-reserved-token anker op
     (progn
       (setf (anker-obj anker) (rest tokens))
@@ -92,45 +129,73 @@
           (nth 1 token))
         (error "数ではありません~%"))))
 
+(defun num (anker)
+  (make-node :kind 'ND_NUM :val (expect-number anker)))
+
 (defun primary (anker)
-  (if (consume anker #\()
+  (if (consume anker "(")
       (prog1
           (expr anker)
-        (expect anker #\)))
-      (make-node :kind 'ND_NUM :val (expect-number anker))))
+        (expect anker ")"))
+      (num anker)))
 
 (defun unary (anker)
   (cond
-    ((consume anker #\+)
+    ((consume anker "+")
      (primary anker))
-    ((consume anker #\-)
+    ((consume anker "-")
      (make-node :kind 'ND_SUB :lhs (make-node :kind 'ND_NUM :val 0) :rhs (primary anker)))
     (t
      (primary anker))))
 
 (defun mul (anker &optional node)
-  (let ((node (if node
+  (let ((left (if node
                   node
                   (unary anker))))
     (cond
-      ((consume anker #\*)
-       (mul anker (make-node :kind 'ND_MUL :lhs node :rhs (unary anker))))
-      ((consume anker #\/)
-       (mul anker (make-node :kind 'ND_DIV :lhs node :rhs (unary anker))))
+      ((consume anker "*")
+       (mul anker (make-node :kind 'ND_MUL :lhs left :rhs (unary anker))))
+      ((consume anker "/")
+       (mul anker (make-node :kind 'ND_DIV :lhs left :rhs (unary anker))))
       (t
-       node))))
+       left))))
 
-(defun expr (anker &optional node)
-  (let ((node (if node
+(defun add-sub (anker &optional node)
+  (let ((left (if node
                   node
                   (mul anker))))
-    (cond
-      ((consume anker #\+)
-       (expr anker (make-node :kind 'ND_ADD :lhs node :rhs (mul anker))))
-      ((consume anker #\-)
-       (expr anker (make-node :kind 'ND_SUB :lhs node :rhs (mul anker))))
-      (t
-       node))))
+    (cond ((consume anker "+")
+           (add-sub anker (make-node :kind 'ND_ADD :lhs left :rhs (mul anker))))
+          ((consume anker "-")
+           (add-sub anker (make-node :kind 'ND_SUB :lhs left :rhs (mul anker))))
+          (t left))))
+
+(defun relational (anker &optional node)
+  (let ((left (if node
+                  node
+                  (add-sub anker node))))
+    (cond ((consume anker "<")
+           (relational anker (make-node :kind 'ND_BIGGER :lhs left :rhs (add-sub anker))))
+          ((consume anker "<=")
+           (relational anker (make-node :kind 'ND_EQ_BIGGER :lhs left :rhs (add-sub anker))))
+          ((consume anker ">")
+           (relational anker (make-node :kind 'ND_BIGGER :lhs (add-sub anker) :rhs left)))
+          ((consume anker ">=")
+           (relational anker (make-node :kind 'ND_EQ_BIGGER :lhs (add-sub anker) :rhs left)))
+          (t left))))
+
+(defun equality (anker &optional node)
+  (let ((left (if node
+                  node
+                  (relational anker))))
+    (cond ((consume anker "==")
+           (equality anker (make-node :kind 'ND_EQUAL :lhs left :rhs (relational anker))))
+          ((consume anker "!=")
+           (equality anker (make-node :kind 'ND_NOT_EQUAL :lhs left :rhs (relational anker))))
+          (t left))))
+
+(defun expr (anker &optional node)
+  (equality anker node))
 
 (defun parse (is)
   (let ((tokens (tokenize is)))
